7a81c5a7d4d322e02ac2a2dc0c24b2bdec900d61
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-11-21 22:16:52 -0300

    feat: :sparkles: resize streetview window and add marker logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import { GoogleMap, StreetViewPanorama, LoadScript, Marker } from '@react-google-maps/api'
+import {
+  GoogleMap,
+  StreetViewPanorama,
+  LoadScript,
+  Marker,
+  Polyline
+} from '@react-google-maps/api'
 import { useState } from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+
+import useStreetView from './hooks/useStreetView'
 
 const containerStyle = {
-  width: '800px',
-  height: '400px'
-};
+  width: '100%',
+  height: '800px'
+}
 
 const initialPosition = {
   lat: -3.745,
   lng: -38.523
-};
+}
+
+const onPolylineLoad = polyline => {
+  console.log('polyline: ', polyline)
+}
+
+const initPolylineOptions = {
+  strokeColor: '#FF0000',
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: '#FF0000',
+  fillOpacity: 0.35,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+  radius: 30000,
+  paths: [
+    {lat: 37.772, lng: -122.214},
+    {lat: 21.291, lng: -157.821},
+    {lat: -18.142, lng: 178.431},
+    {lat: -27.467, lng: 153.027}
+  ],
+  zIndex: 1
+}
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    background-color: black;
+    padding: 0;
+    margin: 0;
+  }
+`
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 8px;
+`
 
 const App = () => {
   const [movements, setMovements] = useState(0)
   const [position, setPosition] = useState(initialPosition)
-  const [streetViewPanorama, setStreetViewPanorama] = useState(undefined)
-
-  const onLoad = (streetViewPanorama) => {
-    console.log('onLoad', streetViewPanorama)
-    setStreetViewPanorama(streetViewPanorama)
-  }
+  const [streetViewPanorama, onLoadStreetView] = useStreetView()
+  const [marker, setMarker] = useState()
+  const [distanceLine, setDistanceLine] = useState(initPolylineOptions)
 
   const onPositionChanged = () => {
     setMovements(movements + 1)
@@ -33,29 +77,59 @@ const App = () => {
     })
   }
 
+  const onMapClick = (cursor) => {
+    console.log(cursor)
+    setMarker({
+      lat: cursor.latLng.lat(),
+      lng: cursor.latLng.lng(),
+    })
+
+    setDistanceLine({
+      ...distanceLine,
+      paths: [
+        initialPosition,
+        {
+          lat: cursor.latLng.lat(),
+          lng: cursor.latLng.lng(),
+        }
+      ]
+    })
+    console.log('dline', distanceLine)
+  }
+
   return (
-    <div>
-      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
-        <GoogleMap mapContainerStyle={containerStyle}>
-          <StreetViewPanorama
-            position={initialPosition}
-            visible={true}
-            onPositionChanged={onPositionChanged}
-            onLoad={onLoad}
-          />
-        </GoogleMap>
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          zoom={12}
-          center={position}
-        >
-          <Marker
-            onLoad={onLoad}
-            position={position}
-          />
-        </GoogleMap>
-      </LoadScript>
-    </div>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
+          <GoogleMap mapContainerStyle={containerStyle}>
+            <StreetViewPanorama
+              position={initialPosition}
+              visible={true}
+              onPositionChanged={onPositionChanged}
+              onLoad={onLoadStreetView}
+            />
+          </GoogleMap>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            zoom={12}
+            center={initialPosition}
+            onClick={onMapClick}
+          >
+            {marker && (
+              <>
+                <Marker position={marker} />
+                <Polyline
+                  onLoad={onPolylineLoad}
+                  path={distanceLine.paths}
+                  options={distanceLine}
+                />
+              </>
+            )}
+          </GoogleMap>
+        </LoadScript>
+      </Wrapper>
+    </>
   )
 };
 
