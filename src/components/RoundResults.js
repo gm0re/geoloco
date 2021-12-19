58663b2be49ac2fde5d0fd2f676b04a19b17f10d
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-12-19 18:04:56 -0300

    feat: :sparkles: modal of results shows round info and results

--- src/components/RoundResults.js
+++ src/components/RoundResults.js
@@ -1,18 +1,130 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import {
+  GoogleMap,
+  Marker,
+  Polygon,
+  Polyline,
+} from '@react-google-maps/api'
+import { Progress, Typography } from 'antd'
+import { round as roundPropTypes } from './propTypes/game'
 
-import gamePropTypes from './propTypes/game'
+const { Title } = Typography
 
-const RoundResults = ({ game }) => {
-  console.log('res', game)
+const mapContainerStyle = {
+  height: 600,
+  width: '100%'
+}
+
+const getScorePercent = (score) => score
+
+const getFormattedDistance = (distance) => {
+  let formattedDistance = {
+    value: parseFloat(distance.toFixed(3)),
+    um: 'm'
+  }
+
+  if (distance >= 1000) {
+    formattedDistance = {
+      value: parseFloat((distance / 1000).toFixed(3)),
+      um: 'km'
+    }
+  }
+
+  if (distance < 1) {
+    formattedDistance = {
+      value: 0,
+      um: 'm'
+    }
+  }
+
+  return formattedDistance
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const RoundResults = ({
+  round,
+  position,
+  site,
+  guessMarker
+}) => {
+  const [formattedDistance, setFormattedDistance] = useState(getFormattedDistance(
+    round.distance
+  ))
+  const [scorePercent] = useState(
+    getScorePercent(round.score) || 50
+  )
+
+  useEffect(() => {
+    setFormattedDistance(getFormattedDistance(
+      round.distance
+    ))
+  }, [round.distance])
+
   return (
-    <div>
-      Distance: {game.rounds[game.rounds.length - 1].distance}
-    </div>
+    <Wrapper>
+      <Title level={2}>
+        Distance: {`${formattedDistance.value} ${formattedDistance.um}`}
+      </Title>
+      <Progress
+        percent={scorePercent}
+        showInfo={false}
+        strokeColor={{
+          from: '#108ee9',
+          to: '#87d068',
+        }}
+      />
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={2}
+        center={position}
+        options={{
+          keyboardShortcuts: false,
+          mapTypeControl: false,
+          streetViewControl: false,
+          zoomControl: true,
+          gestureHandling: 'greedy'
+        }}
+      >
+        {/* just for debugging purpose show polygon: */}
+        {/* <Polygon
+          options={{ clickable: false }}
+          paths={site}
+        /> */}
+        {position && (
+          <Marker position={position} />
+        )}
+        {guessMarker && (
+          <Marker position={guessMarker} />
+        )}
+        {position && guessMarker && (
+          <Polyline
+            path={[position, guessMarker]}
+            options={{ strokeWeight: 2 }}
+          />
+        )}
+      </GoogleMap>
+    </Wrapper>
   )
 }
 
 RoundResults.propTypes = {
-  game: gamePropTypes.isRequired
+  round: roundPropTypes.isRequired,
+  site: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  position: PropTypes.shape({}),
+  guessMarker: PropTypes.shape({})
+}
+
+RoundResults.defaultProps = {
+  position: undefined,
+  guessMarker: undefined
 }
 
 export default RoundResults
