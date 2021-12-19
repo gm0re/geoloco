c5de1802a9c0413f510e050e8b7cbba20600a8dc
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-12-19 18:03:05 -0300

    refactor: :recycle: refactor proptypes and expose rounds props

--- src/components/propTypes/game.js
+++ src/components/propTypes/game.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types'
 
-const game = PropTypes.shape({
-  rounds: PropTypes.arrayOf(PropTypes.shape({
-    score: PropTypes.number,
-    distance: PropTypes.number,
-    length: PropTypes.number
-  })),
+export const round = PropTypes.shape({
+  score: PropTypes.number,
+  distance: PropTypes.number,
+  length: PropTypes.number
+})
+
+export const rounds = PropTypes.arrayOf(round)
+
+export const game = PropTypes.shape({
+  rounds,
   score: PropTypes.number,
   maxRounds: PropTypes.number,
 })
-
-export default game
