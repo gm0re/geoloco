0eeb61910baf47ab8746cdd7cc34f8c52691e695
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-12-25 14:46:55 -0300

    fix: :bug: fix counter to set up a location

--- src/hooks/useStreetViewSvc.js
+++ src/hooks/useStreetViewSvc.js
@@ -6,13 +6,13 @@ const useStreetViewSvc = () => {
   const [streetViewPosition, setStreetViewPosition] = useState()
   const [streetViewSvc, setStreetViewSvc] = useState()
 
-  const setNewStreetViewPosition = (coords) => {
+  const setNewStreetViewPosition = (coords, maxTries = 10) => {
     const MAX_RADIUS = 50
     const MIN_RADIUS = 1000
     // random radius to get random valid location
     const radius = Math.random() * (MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS
 
-    const setNewStreetView = (streetView, status, maxTries = 10) => {
+    const setNewStreetView = (streetView, status) => {
       if (status === SUCCESS) {
         console.log('StreetViewService results', streetView)
         console.log('coords', radius, streetView?.location?.latLng?.lat(), streetView?.location?.latLng?.lng())
@@ -21,12 +21,13 @@ const useStreetViewSvc = () => {
           lat: streetView?.location?.latLng?.lat(),
           lng: streetView?.location?.latLng?.lng()
         })
-      } else {
-        // eslint-disable-next-line no-param-reassign
-        maxTries -= 1
-        console.log('err', status, streetView, maxTries)
-        setNewStreetViewPosition(coords, maxTries)
+      } else if (maxTries > 0) {
+        const newMaxTries = maxTries - 1
+        console.log('err', status, streetView, newMaxTries)
+        setNewStreetViewPosition(coords, newMaxTries)
       }
+      // eslint-disable-next-line
+      return
     }
 
     console.log(streetViewSvc)
