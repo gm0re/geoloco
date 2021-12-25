3659517a3cdc99ecdb3899e3ff7180b9c13c407a
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-12-25 14:46:15 -0300

    feat: :sparkles: change where the files are written

--- geoloco-countries-parser/index.js
+++ geoloco-countries-parser/index.js
@@ -3,52 +3,71 @@ const JSONStream = require('JSONStream');
 const es = require('event-stream');
 
 const getStream = () => {
-    const jsonData = './countries.geojson';
-    const stream = fs.createReadStream(jsonData, { encoding: 'utf8' });
-    const parser = JSONStream.parse('*');
+  const jsonData = './countries.geojson';
+  const stream = fs.createReadStream(jsonData, { encoding: 'utf8' });
+  const parser = JSONStream.parse('*');
 
-    return stream.pipe(parser);
+  return stream.pipe(parser);
 };
 
-getStream().pipe(es.mapSync((data) => {
-  if (Array.isArray(data)) {
-    data.forEach((country) => {
-      const file = `./countries-output/${(country.properties.ADMIN).toLowerCase().replace(' ', '-')}.json`;
+getStream().pipe(es.mapSync((geoData) => {
+  if (Array.isArray(geoData)) {
+    const coordsFormattReducer = (formattedCoords, oldCoords) => {
+      const formatCoords = (coords) => {
+        const getPairCoords = ([lng, lat]) => ({ lat, lng })
+
+        if (!Array.isArray(coords[0])) {
+          return getPairCoords(coords)
+        }
+        return coords.map(getPairCoords)
+      }
+
+      return [
+        ...formattedCoords,
+        formatCoords(oldCoords)
+      ]
+    };
+
+    let countryList = [];
+
+    geoData.forEach((country) => {
+      const countryName = (country.properties.ADMIN).toLowerCase().replace(' ', '-');
+
+      countryList = [
+        ...countryList,
+        countryName
+      ];
+
+      const file = `../public/countries/${countryName}.json`;
 
       console.log(`Writting ${file}`)
 
       // format coords
       const coordinates = country.geometry.coordinates
         .flat(1)
-        .reduce((formattedCoords, oldCoords) => {
-          const formatCoords = (coords) => {
-            getPairCoords = ([lat, lng]) => ({ lat, lng })
-            if (!Array.isArray(coords[0])) {
-              return getPairCoords(coords)
-            }
-            return coords.map(getPairCoords)
-          }
-
-          return [
-            ...formattedCoords,
-            formatCoords(oldCoords)
-          ]
-        }
-      , [])
+        .reduce(coordsFormattReducer, []);
 
       console.log('res', coordinates)
 
       // create country json file with formatted coords
-      fs.writeFile(file, JSON.stringify(coordinates), err => {
+      fs.writeFile(file, JSON.stringify(coordinates), (err) => {
         if (err) {
-          console.error(err)
+          console.error(err);
           return
         }
         console.log(`${file} created succesfully!`)
       })
+    });
+
+    fs.writeFile('../src/constants/countries/index.json', JSON.stringify(countryList, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return
+      }
+      console.log('country list created succesfully!')
     })
   }
-})).on('error', function (err) {
-    // handle any errors
-    console.log('err', err)
+})).on('error', (err) => {
+  // handle any errors
+  console.log('err', err)
 });
