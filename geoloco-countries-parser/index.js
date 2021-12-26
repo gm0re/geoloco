a4becc978a6894dce06457f4328b1674e795779d
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-12-26 17:48:30 -0300

    feat: :sparkles: process the countries geodata filtering by black listed countries

--- geoloco-countries-parser/index.js
+++ geoloco-countries-parser/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const JSONStream = require('JSONStream');
 const es = require('event-stream');
+const blacklistedCountries = require('./blacklistedCountries')
 
 const getStream = () => {
   const jsonData = './countries.geojson';
@@ -12,6 +13,7 @@ const getStream = () => {
 
 getStream().pipe(es.mapSync((geoData) => {
   if (Array.isArray(geoData)) {
+    const getFormattedCountryName = (countryName) => countryName.toLowerCase().replaceAll(' ', '-');
     const coordsFormattReducer = (formattedCoords, oldCoords) => {
       const formatCoords = (coords) => {
         const getPairCoords = ([lng, lat]) => ({ lat, lng })
@@ -29,45 +31,52 @@ getStream().pipe(es.mapSync((geoData) => {
     };
 
     let countryList = [];
+    let blacklistedCount = 0;
 
-    geoData.forEach((country) => {
-      const countryName = (country.properties.ADMIN).toLowerCase().replace(' ', '-');
-
-      countryList = [
-        ...countryList,
-        countryName
-      ];
-
-      const file = `../public/countries/${countryName}.json`;
-
-      console.log(`Writting ${file}`)
-
-      // format coords
-      const coordinates = country.geometry.coordinates
-        .flat(1)
-        .reduce(coordsFormattReducer, []);
-
-      console.log('res', coordinates)
-
-      // create country json file with formatted coords
-      fs.writeFile(file, JSON.stringify(coordinates), (err) => {
-        if (err) {
-          console.error(err);
-          return
+    geoData
+      .filter((country) => {
+        if (blacklistedCountries.includes(getFormattedCountryName(country.properties.ADMIN))) {
+          console.log(`${country.properties.ADMIN} is black listed.`);
+          blacklistedCount += 1;
+          return false;
         }
-        console.log(`${file} created succesfully!`)
+        return true;
       })
-    });
+      .forEach((country) => {
+        const countryName = getFormattedCountryName(country.properties.ADMIN);
+        const file = `../public/countries/${countryName}.json`;
+
+        countryList = [
+          ...countryList,
+          countryName
+        ];
+
+        // format coords
+        const coordinates = country.geometry.coordinates
+          .flat(1)
+          .reduce(coordsFormattReducer, []);
+
+        // create country json file with formatted coords
+        fs.writeFile(file, JSON.stringify(coordinates), (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(`${file} created succesfully!`);
+        })
+      });
+
+    console.log(`countries black listed were ${blacklistedCount} of ${blacklistedCountries.length}.`)
 
     fs.writeFile('../src/constants/countries/index.json', JSON.stringify(countryList, null, 2), (err) => {
       if (err) {
         console.error(err);
-        return
+        return;
       }
-      console.log('country list created succesfully!')
+      console.log('country list created succesfully!');
     })
   }
 })).on('error', (err) => {
   // handle any errors
-  console.log('err', err)
+  console.log('err', err);
 });
