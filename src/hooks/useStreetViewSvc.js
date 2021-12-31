98930039a613c15c2218f9bc9e999290f730b55b
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-12-31 14:54:10 -0300

    fix: :lipstick: remove logs and move sone code lines

--- src/hooks/useStreetViewSvc.js
+++ src/hooks/useStreetViewSvc.js
@@ -1,30 +1,34 @@
 import { useState } from 'react'
 
 const SUCCESS = 'OK'
+const MAX_RADIUS = 50
+const MIN_RADIUS = 1000
 
 const useStreetViewSvc = () => {
   const [streetViewPosition, setStreetViewPosition] = useState()
   const [streetViewSvc, setStreetViewSvc] = useState()
 
   const setNewStreetViewPosition = (coords, maxTries = 10, accRadius = 1) => {
-    const MAX_RADIUS = 50
-    const MIN_RADIUS = 1000
     // random radius to get random valid location
     const radius = (Math.random() * (MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS) * accRadius
 
     const setNewStreetView = (streetView, status) => {
-      console.log('RADIUS', radius)
+      console.log('radius', radius)
       if (status === SUCCESS) {
-        console.log('StreetViewService results', streetView)
-        console.log('coords', radius, streetView?.location?.latLng?.lat(), streetView?.location?.latLng?.lng())
+        console.log(
+          'coords',
+          streetView?.location?.latLng?.lat(),
+          streetView?.location?.latLng?.lng()
+        )
 
         setStreetViewPosition({
           lat: streetView?.location?.latLng?.lat(),
           lng: streetView?.location?.latLng?.lng()
         })
       } else if (maxTries > 0) {
+        console.log('err', status, streetView, maxTries)
+
         const newMaxTries = maxTries - 1
-        console.log('err', status, streetView, newMaxTries)
         setNewStreetViewPosition(coords, newMaxTries, radius)
       }
       // eslint-disable-next-line
@@ -38,7 +42,8 @@ const useStreetViewSvc = () => {
         lat: coords.lat,
         lng: coords.lng
       },
-      radius
+      radius,
+      source: 'outdoor'
     }, setNewStreetView)
   }
 
