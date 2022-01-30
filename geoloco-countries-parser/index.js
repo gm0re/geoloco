aabcb58eb384e232d0efc45b9fe4bb824fc9e724
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2022-01-30 16:08:20 -0300

    feat: :sparkles: add areas calculation per each country

--- geoloco-countries-parser/index.js
+++ geoloco-countries-parser/index.js
@@ -1,82 +1,93 @@
-const fs = require('fs');
-const JSONStream = require('JSONStream');
-const es = require('event-stream');
+const fs = require('fs')
+const JSONStream = require('JSONStream')
+const es = require('event-stream')
+const area = require('area-polygon')
+
 const blacklistedCountries = require('./blacklistedCountries')
 
 const getStream = () => {
-  const jsonData = './countries.geojson';
-  const stream = fs.createReadStream(jsonData, { encoding: 'utf8' });
-  const parser = JSONStream.parse('*');
+  const jsonData = './countries.geojson'
+  const stream = fs.createReadStream(jsonData, { encoding: 'utf8' })
+  const parser = JSONStream.parse('*')
 
-  return stream.pipe(parser);
-};
+  return stream.pipe(parser)
+}
 
 getStream().pipe(es.mapSync((geoData) => {
   if (Array.isArray(geoData)) {
-    const getFormattedCountryName = (countryName) => countryName.toLowerCase().replaceAll(' ', '-');
-    const coordsFormattReducer = (formattedCoords, oldCoords) => {
+    const getFormattedCountryName = (countryName) => countryName.toLowerCase().replaceAll(' ', '-')
+
+    const coordsFormattReducer = (formattedCoords, flatCoords) => {
       const formatCoords = (coords) => {
         const getPairCoords = ([lng, lat]) => ({ lat, lng })
-
+        // country has a single polygon
         if (!Array.isArray(coords[0])) {
           return getPairCoords(coords)
         }
+        // country has many polygons (e.g. islands)
         return coords.map(getPairCoords)
       }
 
+      const getCoordsToCalculateArea = () => (
+        Array.isArray(flatCoords[0][0]) ? flatCoords[0] : flatCoords
+      )
+
       return [
         ...formattedCoords,
-        formatCoords(oldCoords)
+        {
+          coords: flatCoords.map(formatCoords).flat(1),
+          area: area(getCoordsToCalculateArea())
+        }
       ]
-    };
+    }
 
-    let countryList = [];
-    let blacklistedCount = 0;
+    let countryList = []
+    let blacklistedCount = 0
 
     geoData
       .filter((country) => {
         if (blacklistedCountries.includes(getFormattedCountryName(country.properties.ADMIN))) {
-          console.log(`${country.properties.ADMIN} is black listed.`);
-          blacklistedCount += 1;
-          return false;
+          console.log(`${country.properties.ADMIN} is black listed.`)
+          blacklistedCount += 1
+          return false
         }
-        return true;
+        return true
       })
       .forEach((country) => {
-        const countryName = getFormattedCountryName(country.properties.ADMIN);
-        const file = `../public/countries/${countryName}.json`;
+        const countryName = getFormattedCountryName(country.properties.ADMIN)
+        const file = `../public/countries/${countryName}.json`
 
         countryList = [
           ...countryList,
           countryName
-        ];
+        ]
 
         // format coords
-        const coordinates = country.geometry.coordinates
-          .flat(1)
-          .reduce(coordsFormattReducer, []);
+        const coordinates = country.geometry.coordinates.reduce(coordsFormattReducer, [])
+
+        console.log(coordinates)
 
         // create country json file with formatted coords
         fs.writeFile(file, JSON.stringify(coordinates), (err) => {
           if (err) {
-            console.error(err);
-            return;
+            console.error(err)
+            return
           }
-          console.log(`${file} created succesfully!`);
+          console.log(`${file} created succesfully!`)
         })
-      });
+      })
 
     console.log(`countries black listed were ${blacklistedCount} of ${blacklistedCountries.length}.`)
 
     fs.writeFile('../src/constants/countries/index.json', JSON.stringify(countryList, null, 2), (err) => {
       if (err) {
-        console.error(err);
-        return;
+        console.error(err)
+        return
       }
-      console.log('country list created succesfully!');
+      console.log('country list created succesfully!')
     })
   }
 })).on('error', (err) => {
   // handle any errors
-  console.log('err', err);
-});
+  console.log('err', err)
+})
