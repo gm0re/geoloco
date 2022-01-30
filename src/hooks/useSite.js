5c02eb750cf76f0adbb58082a840d2e3210aa7b9
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2022-01-30 16:10:09 -0300

    feat: :sparkles: random site selection is now weighted by area size

--- src/hooks/useSite.js
+++ src/hooks/useSite.js
@@ -7,12 +7,23 @@ const useSite = (country) => {
 
   const randomAreaIndex = Math.floor(Math.random() * countries.length)
 
+  const getRandomSiteIndexBasedOnArea = (countryBounds) => {
+    const areas = countryBounds.map(({ area }) => area)
+    // eslint-disable-next-line
+    const areasWeights = areas.map((sum => value => sum += value)(0))
+    const randomIndex = Math.floor(Math.random() * areasWeights[areasWeights.length - 1])
+    const randomSiteIndex = areasWeights.filter((areaWeight) => randomIndex >= areaWeight).length
+    console.log('areas', areas, areasWeights, randomSiteIndex)
+    // returns higher index or area where the random is bigger or equal
+    return randomSiteIndex
+  }
+
   const setCountryBounds = (countryBounds) => {
     let selectedBounds = countryBounds
     // found a multi polygon country
-    if (Array.isArray(countryBounds[0])) {
+    if (Array.isArray(countryBounds[0].coords)) {
       // get one of the polygons randomly
-      selectedBounds = countryBounds[Math.floor(Math.random() * countryBounds.length)]
+      selectedBounds = countryBounds[getRandomSiteIndexBasedOnArea(countryBounds)].coords
     }
     setSite(selectedBounds)
   }
@@ -31,7 +42,7 @@ const useSite = (country) => {
       }))
       .catch((error) => {
         console.log(error)
-      });
+      })
   }
 
   const setRandomSite = () => {
